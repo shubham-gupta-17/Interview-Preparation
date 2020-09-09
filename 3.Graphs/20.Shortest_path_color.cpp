43c4e28b7ae28898dd722a3ea9b02aee04c96f31
Shubham <sg61642@gmail.com>
2020-09-09 20:28:47 +0530

    update

---
+++ 3.Graphs/20.Shortest_path_color.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+// leetcode 1129
+// 1=red,0=blue
+class Node{
+    public :
+    int u;
+    int color;
+    Node(int v,int c){
+        u=v;
+        color=c;
+    }
+};
+vector<int> ans;
+int find(vector<vector<Node>>&graph,int source,int destination)
+{
+     queue<Node> que;
+     for(Node node:graph[source])
+     {
+         que.push(node);
+     }
+     int dis=1;
+     while(!que.empty())
+     {
+         int size=que.size();
+         while(size--)
+         {
+             Node node=que.front();
+             que.pop();
+             if(node.u==destination) return dis;
+             for(Node root:graph[node.u])
+             {
+                 if(root.color==node.color)  continue;
+                 que.push(root);
+
+             }
+         }
+         dis++;
+     }
+    return -1;
+}
+vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red, vector<vector<int>>& blue) {
+    if(n==0) return ans;
+    ans.push_back(0);
+     vector<vector<Node>> graph(n,vector<Node>());
+    for(int i=0;i<red.size();i++)
+    {
+        int u=red[i][0];
+        int v=red[i][1];
+        Node node(v,1);
+        graph[u].push_back(node);
+    }
+    for(int i=0;i<blue.size();i++)
+    {
+        int u=blue[i][0];
+        int v=blue[i][1];
+        Node node(v,0);
+        graph[u].push_back(node);
+    }
+
+    for(int i=1;i<n;i++)
+    {
+        ans.push_back(find(graph,0,i));
+    }
+    return ans;
+
+
+}
+int main()
+{
+    int n=3;
+    vector<vector<int>>red={{0,1},{0,2}};
+    vector<vector<int>>blue={{1,0}};
+
+    shortestAlternatingPaths(n,red,blue);
+    for(int a:ans)cout<<a<<" ";
+    return 0;
+}
--- 9.Stack and Queue/1.Maximum_width_ramp.cpp
+++ 9.Stack and Queue/1.Maximum_width_ramp.cpp
@@ -1,7 +1,83 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 // leetcode 962
+//Resource link:https://leetcode.com/problems/maximum-width-ramp/discuss/265765/Detailed-Explaination-of-all-the-three-approaches
+
+// =======================================================
+int maxWidthRamp(vector<int>& A) {
+        int n=A.size();
+        if(n==0) return 0;
+        int ans=0;
+        for(int i=0; i<n; i++)
+        {
+                for(int j=n-1; j>i; j--)
+                {
+                        if(A[i]<=A[j]) {
+                                ans=max(ans,j-i);
+                                break;
+                        }
+                }
+        }
+        return ans;
+}
+//============================================================
+int maxWidthRamp_sort(vector<int>&a)
+{
+        int n=a.size();
+        if(n==0) return 0;
+        vector<vector<int> > idx(n,vector<int>(2,0));
+        for(int i=0; i<n; i++)
+        {
+                idx[i][0]=a[i];
+                idx[i][1]=i;
+        }
+
+        sort(idx.begin(),idx.end());
+        int mn=n-1;
+        int ans=0;
+        for(int i=0; i<n; i++)
+        {
+                mn=min(mn,idx[i][1]);
+                ans=max(ans,idx[i][1]-mn);
+        }
+        return ans;
+}
+
+//=================================================================
+int maxWidthRamp_stack(vector<int>&A)
+{
+        int n=A.size();
+        if(n==0) return 0;
+        stack<int> st;
+        for(int i=0; i<n; i++)
+        {
+                if(st.empty() || A[i]<A[st.top()])
+                        st.push(i);
+        }
+
+        int ans=0;
+        for(int i=n-1; i>=0; i--)
+        {
+                while(!st.empty() && A[i]>=A[st.top()])
+                {
+                        ans=max(ans,i-st.top());
+                        st.pop();
+                }
+        }
+        return ans;
+}
 int main()
 {
+        vector<int> a={9,8,1,0,1,9,4,0,4,1};
+        // bruteforce will give TLE O(n^2)
+        cout<<maxWidthRamp(a)<<endl;
 
+        // using index and sort O(nlogn)
+
+        cout<< maxWidthRamp_sort(a)<<endl;
+
+        // best using stack O(n)
+        cout<<maxWidthRamp_stack(a);
+
+        return 0;
 }
