a09cbe33051641c2916577a7294aa67a2d1dce0b
shubham-gupta-17 <39772611+shubham-gupta-17@users.noreply.github.com>
2020-09-19 17:10:27 +0000

    update

--- 2.Dynamic_Programming/8.Egg_drop.cpp
+++ 2.Dynamic_Programming/8.Egg_drop.cpp
@@ -14,10 +14,36 @@ using namespace std;
 }
 
 // Approach 2: Binary Search
+int f(int x, int K, int N) {
+        int ans = 0, r = 1;
+        for (int i = 1; i <= K; ++i) {
+            r *= x-i+1;
+            r /= i;
+            ans += r;
+            if (ans >= N) break;
+        }
+        return ans;
+}
+
+int superEggDrop_2(int K, int N) {
+        int lo = 1, hi = N;
+        while (lo < hi) {
+            int mi = (lo + hi) / 2;
+            if (f(mi, K, N) < N)
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+
+        return lo;
+}
+
 int main()
 {
     int k;
     int n;
     cin>>k>>n;
     cout<<superEggDrop(k,n)<<endl;
+    cout<<superEggDrop_2(k,n);
+    return 0;
 }
