b14c5b7dadf4e7a4398b78472240335bcc0f14b6
shubham-gupta-17 <39772611+shubham-gupta-17@users.noreply.github.com>
2020-09-19 11:09:43 +0000

    worked on questions using sliding window

--- 8.Deque/2.Nice_Subarray.cpp
+++ 8.Deque/2.Nice_Subarray.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 //leetcode 1248
 
-// Approach 1:O(n) time and O(1) space
+// Approach 1:O(n) time and O(1) space 
+// Replace even with 0 and odd with 1 the question becomes find subarray with sum equal k
+int numberOfSubarrays(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==0) return 0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]%2==0) nums[i]=0;
+            else nums[i]=1;
+        }
+        unordered_map<int,int> map;
+        map[0]=1;
+        int sum=0;
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=nums[i];
+            if(map.find(sum-k)!=map.end()) count+=map[sum-k];
+            map[sum]+=1;
+        }
+        
+        return count;
+}
 // Approach 2: O(n) time and O(1) space
 int numberOfSubarrays(vector<int>& nums, int k) {
        int n=nums.size();
