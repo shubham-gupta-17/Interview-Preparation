4c1fcfd0a7c3f9c77131b41f0f34979e9e033875
shubham-gupta-17 <39772611+shubham-gupta-17@users.noreply.github.com>
2020-09-19 14:10:18 +0000

    update

--- 8.Deque/2.Nice_Subarray.cpp
+++ 8.Deque/2.Nice_Subarray.cpp
@@ -27,7 +27,7 @@ int numberOfSubarrays(vector<int>& nums, int k) {
         return count;
 }
 // Approach 2: O(n) time and O(1) space
-int numberOfSubarrays(vector<int>& nums, int k) {
+int numberOfSubarrays_opti(vector<int>& nums, int k) {
        int n=nums.size();
        if(n==0) return 0;
        int i=0;
@@ -49,4 +49,29 @@ int numberOfSubarrays(vector<int>& nums, int k) {
         }
         return ans;
 
+}
+
+// Approach 3: O(n) time and O(k) space
+int numberofSubarrays_3(vector<int>&nums,int k)
+{
+    int n=nums.size();
+    if(n==0) return 0;
+
+    deque<int> deq={-1};
+    int ans=0;
+    for(int i=0;i<n;i++)
+    {
+        if(nums[i]%2) deq.push_back(i);
+        if(deq.size()>k+1) deq.pop_front();
+        if(deq.size()==k+1) ans+=deq[1]-deq[0];
+
+    }
+    return ans;
+}
+int main()
+{
+    vector<int> arr={2,2,2,1,2,2,1,2,2,2};
+    int k=2;
+    cout<<numberOfSubarrays(arr,k)<<endl;
+    cout<<numberOfSubarrays_opti(arr,k)<<endl;
 }
