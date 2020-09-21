3b8f7c9bac496005acf2db8dca60aa4e6c9658bf
Shubham <sg61642@gmail.com>
2020-09-21 13:30:59 +0530

    update

--- 3.Graphs/17.MaxArea.cpp
+++ 3.Graphs/17.MaxArea.cpp
@@ -34,14 +34,88 @@ int maxAreaOfIsland(vector<vector<int>>& grid) {
 
 //====================================================================================
 // Approach 2: union find
-
-int max_area_union(vector<vector<char>>&gird)
+vector<int>parent;
+vector<int>size;
+int n,m;
+int par(int u)
 {
+  if(parent[u]==u) return u;
+  return parent[u]=par(parent[u]);
+}
 
+void merge(int p1,int p2)
+{
+   if(size[p1]<size[p2])
+   {
+     parent[p1]=p2;
+     size[p2]+=size[p1];
+   }
+   else{
+     parent[p2]=p1;
+     size[p1]+=size[p2];
+   }
+   return ;
+}
+int max_area_union(vector<vector<int>>&grid)
+{
+    n=grid.size();
+    if(n==0) return 0;
+    m=grid[0].size();
+    parent.resize(n*m,-1);
+    size.resize(n*m,1);
+    // set parent of all
+    for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<m;j++)
+        parent[i*m+j]=i*m+j;
+    }
+
+    // union findParent
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+          if(grid[i][j]==1){
+            int p1=par(i*m+j);
+            if(i+1<n && grid[i+1][j]==1)
+            {
+                int p2=par((i+1)*m+j);
+                if(p1!=p2)
+                  merge(p1,p2);
+            }
+
+            if(j+1<m && grid[i][j+1]==1)
+            {
+              int p2=par(i*m+(j+1));
+              if(p1!=p2) merge(p1,p2);
+            }
+
+          }
+        }
+    }
+
+  int max_size=0;
+    for(int i=0;i<(n*m);i++)
+    {
+        int r=(i/m);
+        int c=(i%m);
+      if(parent[i]==i && grid[r][c]==1){
+      max_size=max(size[i],max_size);
+
+      }
+    }
+    return max_size;
 }
 int main()
 {
-  vector<vector<char>> grid={{"1","1","0","0","0"},{"1","1","0","0","0"},{"0","0","1","0","0"},{"0","0","0","1","1"}};
+  vector<vector<char>> grid={{0,0,1,0,0,0,0,1,0,0,0,0,0},
+ {0,0,0,0,0,0,0,1,1,1,0,0,0},
+ {0,1,1,0,1,0,0,0,0,0,0,0,0},
+ {0,1,0,0,1,1,0,0,1,0,1,0,0},
+ {0,1,0,0,1,1,0,0,1,1,1,0,0},
+ {0,0,0,0,0,0,0,0,0,0,1,0,0},
+ {0,0,0,0,0,0,0,1,1,1,0,0,0},
+ {0,0,0,0,0,0,0,1,1,0,0,0,0}};
   cout<<maxAreaOfIsland(grid);
   return 0;
 }
