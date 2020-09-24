a2d63d7ebd4cb246e4b0f06d391394134ded3d5b
Shubham <sg61642@gmail.com>
2020-09-24 21:06:58 +0530

    update

--- 12.Placement/1.Ama/37.top_K_freq.cpp
+++ 12.Placement/1.Ama/37.top_K_freq.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define pp pair<string,int>
-
+// Approach 1:
 vector<string> topKFrequent(vector<string>& words, int k) {
        int n=words.size();
        vector<string> ans;
@@ -30,10 +30,44 @@ vector<string> topKFrequent(vector<string>& words, int k) {
        return ans;
 
 }
+//===============================================================================
+struct compare{
+        bool operator()(pp a,pp b){
+           if(a.second==b.second) return a.first < b.first;
+           return a.second > b.second;
+        }
+    };
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        int n=words.size();
+        vector<string> ans(k,"");
+        if(n==0) return ans;
+
+        map<string,int>map;
+        for(string &s:words)
+        {
+             map[s]+=1;
+        }
+
+       priority_queue<pp, vector<pp>, compare> que;
+
+        for(auto it:map)
+        {
+            que.push(it);
+            if(que.size()>k) que.pop();
+        }
+
+        for(int i=0;i<k;i++)
+        {
+            pp top=que.top();
+            ans.push_back(top.first);
+            que.pop();
+        }
+        return ans;
+
+    }
 int main()
 {
    vector<string> arr={"i", "love", "leetcode", "i", "love", "coding"};
    topKFrequent(arr,2);
    return 0;
 }
-
