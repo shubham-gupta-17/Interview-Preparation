c1bdb593538ca66a595a3cce748d1bccbc0b54e5
Shubham <sg61642@gmail.com>
2020-10-09 13:20:47 +0530

    update

---
+++ 17.Bit/1.Reverse_Bits.cpp
@@ -0,0 +1,22 @@
+ #include<bits/stdc++.h>
+using namespace std;
+// leetcode  190
+
+ uint32_t reverseBits(uint32_t n) {
+        int res=0;
+        if(n==0) return 0;
+       
+        for(int i=0;i<32;i++){
+            
+            res <<= 1;
+            if((n&1)) res|=1;
+            
+            n>>=1;
+        }
+        return res;
+ }
+ int main()
+ {
+ 	reverseBits(00000010100101000001111010011100);
+ 	return 0;
+ }
