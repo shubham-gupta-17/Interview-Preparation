2c5bc7fe6b155a2ab76ebdd27ff555b631d81fbd
Shubham <sg61642@gmail.com>
2020-10-18 07:47:05 +0530

    update

--- 17.Bit/6.Hamming_Distance.cpp
+++ 17.Bit/6.Hamming_Distance.cpp
@@ -3,9 +3,9 @@ using namespace std;
 // leetcode 477
 int hammingDistance2(int x,int y){
 int dist=0;
-int n=(x^y);
+int n=(x^y);  // xor will set the bit of n which is different in both x and y
 while(n>0){
-    n&=(n-1);
+    n&=(n-1);  // count no of set bits in n
     dist++;
 }
 return dist;
@@ -23,7 +23,24 @@ int totalHammingDistance1(vector<int>& nums) {
 }
 //=====================================================================================
 int totalHammingDistance2(vector<int>&nums){
-    
+         int n=nums.size();
+        if(n<2) return 0;
+        int *a=new int[2]();
+        int count=0;
+        int ans=0;
+        while(1){
+            a[0]=0;
+            a[1]=0;
+            count=0;
+            for(int i=0;i<n;i++){
+                if(nums[i]==0) count++;
+                a[nums[i]%2]++;
+                nums[i]>>=1;
+            }
+            ans+=a[0]*a[1];
+             if(count==n) return ans;
+        }
+        return ans;
 }
 int main(){
     vector<int> nums={4,2,5,6,10};
