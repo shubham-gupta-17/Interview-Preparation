11bd7d166fb71747dcc44f7e288af39f43368f9b
Shubham <sg61642@gmail.com>
2020-10-21 08:27:18 +0530

    update

--- 13.Priority_queue/4.K_points_origin.cpp
+++ 13.Priority_queue/4.K_points_origin.cpp
@@ -1,35 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
 // leetcode 973
- class point{
-        public:
-        int x=0;
-        int y=0;
-        double dist=0;
-        point(int x1,int y1){
-            x=x1;
-            y=y1;
-            dist=sqrt((x*x) + (y*y));
-        }  
-    };
-    class compare{
-        public:
-        bool operator()(point p1,point p2){
-            return p1.dist > p2.dist;
+class point{
+    public:
+    int x=0;
+    int y=0;
+    double dist=0;
+    point(int x1,int y1){
+        x=x1;
+        y=y1;
+        dist=sqrt((x*x) + (y*y));
+    }  
+};
+class compare{
+    public:
+    bool operator()(point p1,point p2){
+        return p1.dist > p2.dist;
+    }
+};
+vector<vector<int>>ans;
+vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+    priority_queue<point,vector<point>,compare> que;
+    
+    for(vector<int> arr:points){
+        que.push({arr[0],arr[1]});
+    }
+    while(K--){
+        ans.push_back({que.top().x,que.top().y});
+        que.pop();
+    }
+    return ans;
+}
+//================================================================================
+    // Quick Select Algorithm
+int distance(vector<int>& p) {
+        return p[0]*p[0] + p[1]*p[1];
+    }
+    
+    int partition(vector<vector<int>>& arr, int low, int high) {
+        int pivot = distance(arr[high]);
+        for (int i = low; i < high; ++i) {
+            if(distance(arr[i]) < pivot)
+                swap(arr[i], arr[low++]);
         }
-    };
-    vector<vector<int>>ans;
+        swap(arr[high], arr[low]);
+        return low;
+    }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        priority_queue<point,vector<point>,compare> que;
-        
-        for(vector<int> arr:points){
-            que.push({arr[0],arr[1]});
+        int l = 0, r = points.size() - 1;
+        --K;
+        while(true) {
+            int p = partition(points, l, r);
+            if (p == K)
+                break;
+            if (p < K) {
+                l = p + 1;
+            } else {
+                r = p - 1;
+            }
         }
-        while(K--){
-            ans.push_back({que.top().x,que.top().y});
-            que.pop();
-        }
-        return ans;
+        return vector<vector<int>>(points.begin(), points.begin() + K + 1);
     }
 int main(){
 
