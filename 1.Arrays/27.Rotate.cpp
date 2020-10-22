7ce20227001f010fc1be7abfaed752de7b544f10
Shubham <sg61642@gmail.com>
2020-10-22 22:48:55 +0530

    update

---
+++ 1.Arrays/27.Rotate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// leetcode 48
+void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(),matrix.end());
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i+1;j<matrix.size();j++)
+                swap(matrix[i][j],matrix[j][i]);
+        }
+        return ;
+}
+int main(){
+    
+}
