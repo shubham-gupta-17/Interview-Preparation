7726cb8c8fd5da3d911a44b4f5f9a3f038ba37e7
Shubham <sg61642@gmail.com>
2020-10-23 08:15:15 +0530

    update

--- 1.Arrays/29.Rotate_Array.cpp
+++ 1.Arrays/29.Rotate_Array.cpp
@@ -35,7 +35,26 @@ reverse the elements 4,3,2,1
 you will get[5,6,7,1,2,3,4]
 
 */
-
+void reverse(vector<int>&nums,int start,int end){
+        while(start<=end)
+            swap(nums[start++],nums[end--]);
+        return ;
+}
+void rotate(vector<int>& nums, int k) {
+    int n=nums.size();
+    if(k==0 || !(k%n)) return ;
+    if(k>n) k%=n;
+    
+    // reverse all nums
+    reverse(nums,0,n-1);
+    
+    // reverse from 0 to k-1
+    reverse(nums,0,k-1);
+    
+    // reverse from k to n-1;
+    reverse(nums,k,n-1);
+    return ;
+}
 
 int main(){
     vector<int> a={1,2,3,45,6};
