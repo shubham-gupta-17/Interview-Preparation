ef68f493bc0c215a4c665fd5bae3fb97e3695458
Shubham <sg61642@gmail.com>
2020-10-23 08:14:46 +0530

    update

--- 1.Arrays/29.Rotate_Array.cpp
+++ 1.Arrays/29.Rotate_Array.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+// ======================================================================
+// Approach 1: O(n*k) time and O(1) space
+
  void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         if(k>n) k=(k%n);
@@ -13,6 +16,27 @@ using namespace std;
         }
         return ;
 }
+
+//=====================================================================================
+// Approach 2:
+/*
+    let a= [1,2,3,4,5,6,7]
+k = 3.
+
+we have to first reverse the whole array by swapping first element with the last one and so on..
+you will get[7,6,5,4,3,2,1]
+
+reverse the elements from 0 to k-1
+reverse the elements 7,6,5
+you will get [5,6,7,4,3,2,1]
+
+reverse the elements from k to n-1
+reverse the elements 4,3,2,1
+you will get[5,6,7,1,2,3,4]
+
+*/
+
+
 int main(){
     vector<int> a={1,2,3,45,6};
     rotate(a,13);
