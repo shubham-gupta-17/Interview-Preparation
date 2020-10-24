048de729306e71bd7cc6d48439cc6abb997e89c5
Shubham <sg61642@gmail.com>
2020-10-24 21:57:07 +0530

    update

--- 21.Two Pointers/6.Container_water.cpp
+++ 21.Two Pointers/6.Container_water.cpp
@@ -1,2 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+// leetcode 11
+int maxArea(vector<int>& A) {
+        int S = 0, i = 0, j = A.size() -1;
+        while (i < j) {
+            S = max(S, (j - i) *min(A[i], A[j]));
+            if (A[i] < A[j]) i++; else j--;
+        }
+        return S;
+}
+int main(){
+    vector<int> arr={1,2,1};
+    cout<<maxArea(arr);
+    return 0;
+}
