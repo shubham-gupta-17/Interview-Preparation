379ea2b36df1fc8064c822d074c44c6aa4ed26a0
Shubham <sg61642@gmail.com>
2020-10-26 16:12:03 +0530

    update

---
+++ 1.Arrays/34.Set_Matrix.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+// leetcode 73
+int n;
+int m;
+void setZero(vector<vector<int>> &grid, int x, int y)
+{
+    for (int j = 0; j < m; j++)
+        grid[x][j] = 0;
+
+    for (int i = 0; i < n; i++)
+        grid[i][y] = 0;
+
+    return;
+}
+void setZeroes(vector<vector<int>> &matrix)
+{
+    n = matrix.size();
+    m = matrix[0].size();
+    vector<vector<int>> vis;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                vis.push_back({i, j});
+            }
+        }
+    }
+
+    for (vector<int> dir : vis)
+        setZero(matrix, dir[0], dir[1]);
+
+    return;
+}
+int main()
+{
+}
