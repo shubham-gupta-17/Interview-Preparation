74e2988cf4322e7ef14dd0ae7ee4bd17f24d5d1a
Shubham <sg61642@gmail.com>
2020-11-03 00:05:19 +0530

    update

--- 7.Linked List/3.Reverse.cpp
+++ 7.Linked List/3.Reverse.cpp
@@ -1,36 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 // leetcode 209
-class ListNode{
+class ListNode
+{
 
-    public:
+public:
     int val;
-    ListNode *next=NULL;
-    ListNode(int x){
-        val=x;
+    ListNode *next = NULL;
+    ListNode(int x)
+    {
+        val = x;
     }
 };
- ListNode *head1;
-    ListNode *reverse(ListNode *head){
-        ListNode *node;
-        if(!head->next){
-          node=new ListNode(head->val);
-          head1=node;
-            return node;
-        }
-        node=reverse(head->next);
-        node->next=new ListNode(head->val);
-        return node->next;
+ListNode *head1;
+ListNode *reverse(ListNode *head)
+{
+    ListNode *node;
+    if (!head->next)
+    {
+        node = new ListNode(head->val);
+        head1 = node;
+        return node;
     }
-    ListNode* reverseList(ListNode* head) {
-        if(!head) return head;
-        reverse(head);
-        return head1;
-    }
-int main(){
-	ListNode *head=new ListNode(1);
-	head->next=new ListNode(2);
-	head->next->next=new ListNode(3);
-	ListNode *node=reverseList(head);
-	return 0;	
+    node = reverse(head->next);
+    node->next = new ListNode(head->val);
+    return node->next;
+}
+ListNode *reverseList(ListNode *head)
+{
+    if (!head)
+        return head;
+    reverse(head);
+    return head1;
+}
+int main()
+{
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    ListNode *node = reverseList(head);
+    return 0;
 }
