91092f25611f06f8ab88216c4184518504904ed5
Shubham <sg61642@gmail.com>
2020-11-03 18:20:11 +0530

    update

--- 11.Sliding Window/4.Anagrams_String.cpp
+++ 11.Sliding Window/4.Anagrams_String.cpp
@@ -46,6 +46,43 @@ vector<int> findAnagrams(string s, string p)
 
     return ans;
 }
+//=====================================================================================
+vector<int> findAnagrams(string s, string p)
+{
+    int n = s.length();
+    int m = p.length();
+    vector<int> ans;
+    if (n == 0)
+        return ans;
+    if (n < m)
+        return ans;
+
+    unordered_map<char, int> mapp;
+    for (char ch : p)
+        mapp[ch]++;
+
+    unordered_map<char, int> maps;
+    int start = 0, end = 0;
+
+    while (end < n)
+    {
+        maps[s[end++]]++;
+        m--;
+        if (m == 0)
+        {
+            if (maps == mapp)
+                ans.push_back(start);
+            if (maps[s[start]] == 1)
+                maps.erase(s[start]);
+            else
+                maps[s[start]]--;
+            start++;
+            m++;
+        }
+    }
+
+    return ans;
+}
 int main()
 {
     string s = "cbaacbcbdfsacb";
