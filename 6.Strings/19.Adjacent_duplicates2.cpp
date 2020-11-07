22fea1a9c6c202d27667546a0b691c205bfe5ecc
Shubham <sg61642@gmail.com>
2020-11-07 23:48:23 +0530

    update

--- 6.Strings/19.Adjacent_duplicates2.cpp
+++ 6.Strings/19.Adjacent_duplicates2.cpp
@@ -2,28 +2,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 // leetcode 1209
-string removeDuplicates(string s, int k) {
-        int n=s.length();
-        int start=0,end=0;
-        stack<pair<char,int>> st;
-        for(char ch:s){
-            if(st.empty() || st.top().first!=ch) st.push({ch,1});
-            else st.top().second++;
-            
-            if(st.top().second==k) st.pop();
-        }
-        
-        string ans="";
-        while(!st.empty()){
-            auto it=st.top();
-            ans.append(it.second,it.first);
+string removeDuplicates(string s, int k)
+{
+    int n = s.length();
+    int start = 0, end = 0;
+    stack<pair<char, int>> st;
+    for (char ch : s)
+    {
+        if (st.empty() || st.top().first != ch)
+            st.push({ch, 1});
+        else
+            st.top().second++;
+
+        if (st.top().second == k)
             st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        
-        return ans;
-        
     }
+
+    string ans = "";
+    while (!st.empty())
+    {
+        auto it = st.top();
+        ans.append(it.second, it.first);
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+
+    return ans;
+}
 int main()
 {
     string s;
