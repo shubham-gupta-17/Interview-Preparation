43289ddbd6288848407f63e669ef3455be7180b2
Shubham <sg61642@gmail.com>
2020-11-07 21:11:38 +0530

    update

---
+++ 6.Strings/18.Adjacent_duplicates.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+// leetcode 1047
+string removeDuplicates(string &s) {
+        int n=s.length();
+        if(n==0) return s;
+	    stack<char>st;
+        for(int i=0;i<n;i++){
+            bool flag=false;
+            while(!st.empty() && st.top()==s[i]){
+                flag=true;
+                st.pop();
+            }
+            if(!flag) st.push(s[i]);
+        }
+        
+        string ans="";
+        while(!st.empty()){
+            char ch=st.top();
+            st.pop();
+            ans+=ch;
+        }
+         reverse(ans.begin(),ans.end());
+        return ans;
+    }
+int main(){
+    string s;
+    cin>>s;
+    cout<<removeDuplicates(s);
+    return 0;
+}
