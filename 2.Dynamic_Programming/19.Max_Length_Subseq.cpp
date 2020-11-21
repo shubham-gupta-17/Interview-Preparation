cc2b43796ef6aa2efce8329a71c3867e093d9ffc
Shubham <sg61642@gmail.com>
2020-11-21 23:06:02 +0530

    Solved questions of DP

--- 2.Dynamic_Programming/19.Max_Length_Subseq.cpp
+++ 2.Dynamic_Programming/19.Max_Length_Subseq.cpp
@@ -15,12 +15,30 @@ int find_Subseq(int n,int arr[]){
     }
     return ans;
 }
+
+// Approach 2: Hashmap : O(n) time and space
+int find_Subseq_map(int n,int arr[]){
+    unordered_map<int,int> map;
+    // search for arr[i],arr[i+1],arr[i-1] and store max length against arr[i];
+    int ans=0;
+    for(int i=0;i<n;i++){
+        int num=arr[i];
+        int len=0;
+        if(map.find(num)!=map.end()) len=map[num];
+        if(map.find(num-1)!=map.end()) len=max(len,map[num]);
+        if(map.find(num+1)!=map.end()) len=max(len,map[num+1]);
+        map[num]=len+1;
+        ans=max(ans,len+1);
+    }
+    return ans;
+}
 int main(){
     int n;
     cin>>n;
 	int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
 
-    cout<<find_Subseq(n, arr);
+    cout<<find_Subseq(n, arr)<<endl;
+    cout<<find_Subseq_map(n,arr);
 	return 0;
 }
