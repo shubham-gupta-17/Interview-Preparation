6ac4c2da47b049f50ae2a8c264bc9e1eb02da997
Shubham <sg61642@gmail.com>
2020-11-21 22:37:18 +0530

    Solved questions of DP

---
+++ 2.Dynamic_Programming/19.Max_Length_Subseq.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+// https://www.geeksforgeeks.org/maximum-length-subsequence-difference-adjacent-elements-either-0-1/
+int find_Subseq(int n,int arr[]){
+    int *dp=new int[n]();
+    dp[0]=1;
+    int ans=0;
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            int diff=abs(arr[i]-arr[j]);
+            if(diff==0 || diff=1) 
+                dp[i]=max(dp[i],dp[j]+1);
+        }
+        ans=max(ans,dp[i]);
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+	int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+
+    cout<<find_Subseq(n, arr);
+	return 0;
+}
