e18ed0cab837b566e8882c491ac678c34ad1093d
Shubham <sg61642@gmail.com>
2020-11-22 10:02:53 +0530

    Solved questions of Arrays

--- 1.Arrays/37.Gas_station.cpp
+++ 1.Arrays/37.Gas_station.cpp
@@ -28,6 +28,28 @@ int n;
         }
         return -1;
     }
+    
+//=============================================================================================
+ int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        n=gas.size();
+        for(int i=0;i<n;i++){
+            gas.push_back(gas[i]);
+            cost.push_back(cost[i]);
+        }
+        int start=0;
+        int end=0;
+        int fuel=0;
+        while(end<gas.size()){
+            fuel+=gas[end];
+            if(end==n+start) return start;
+            // go to next station till fuel is greater than zero
+            fuel-=cost[end];
+            end++;
+            // if fuel is less then 0 then start adding from start till it does not become greater then 0
+            while(start<end && fuel<0) fuel=fuel+cost[start] - gas[start++];
+        }
+        return -1;
+    }
 int main(){
 	vector<int> gas={2,3,4};
 	vector<int> cost={3,4,3};
