aaba42f9f32a1e656c03433c20d110674c9b13f3
Shubham <sg61642@gmail.com>
2020-11-26 05:25:20 +0530

    Solved questions

--- 12.Placement/3.Expedia/1.Subarray_product_less_k.cpp
+++ 12.Placement/3.Expedia/1.Subarray_product_less_k.cpp
@@ -1,19 +1,24 @@
 #include <bits./stdc++.h>
 using namespace std;
 // leetcode 713
-int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(n==0) return 0;
-        int count=0;
-        for(int i=0;i<n;i++){
-            long long prod=1;
-            for(int j=i;j<n;j++){
-                prod*=nums[j];
-                if(prod<k) count++;
-            }
+int numSubarrayProductLessThanK(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    if (n == 0)
+        return 0;
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        long long prod = 1;
+        for (int j = i; j < n; j++)
+        {
+            prod *= nums[j];
+            if (prod < k)
+                count++;
         }
-        return count;
     }
+    return count;
+}
 //===========================================================================
 int numSubarrayProductLessThanK(vector<int> &nums, int k)
 {
