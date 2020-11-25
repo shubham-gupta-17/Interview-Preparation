5cb4ff9eec2a9fb232035eefa5597b838000e900
Shubham <sg61642@gmail.com>
2020-11-26 05:22:35 +0530

    Solved questions

--- 12.Placement/3.Expedia/1.Subarray_product_less_k.cpp
+++ 12.Placement/3.Expedia/1.Subarray_product_less_k.cpp
@@ -1,23 +1,27 @@
-#include<bits./stdc++.h>
+#include <bits./stdc++.h>
 using namespace std;
-// leetcode 713 
-int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(n==0) return 0;
-        int start=0,end=0;
-        long long prod=1;
-        int count=0;
-        while(end<n){
-            prod*=nums[end];
-                while(start<=end && prod>=k)
-                prod/=nums[start++];
-            count+=(end-start+1);
-            end++;
-        }
-        return count;
+// leetcode 713
+int numSubarrayProductLessThanK(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    if (n == 0)
+        return 0;
+    int start = 0, end = 0;
+    long long prod = 1;
+    int count = 0;
+    while (end < n)
+    {
+        prod *= nums[end];
+        while (start <= end && prod >= k)
+            prod /= nums[start++];
+        count += (end - start + 1);
+        end++;
     }
-   int main(){
-   	vector<int> ar={10,2,5,6};
-    cout<<numSubarrayProductLessThanK(ar,100);
-   	return 0;
-   }
+    return count;
+}
+int main()
+{
+    vector<int> ar = {10, 2, 5, 6};
+    cout << numSubarrayProductLessThanK(ar, 100);
+    return 0;
+}
