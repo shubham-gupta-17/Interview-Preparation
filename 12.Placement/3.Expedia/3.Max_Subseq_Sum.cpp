f35edbb56581283d3d1d047cf6766736b2cbace5
Shubham <sg61642@gmail.com>
2020-11-26 09:33:16 +0530

    Solved questions

--- 12.Placement/3.Expedia/3.Max_Subseq_Sum.cpp
+++ 12.Placement/3.Expedia/3.Max_Subseq_Sum.cpp
@@ -20,6 +20,22 @@ int find(vector<int>ar,int sum,int n,int count,int idx){
 	    ans=max(ans,find(arr,arr[i],n,1,i));
 	    return ans;
 	}  
+
+//=================================================================
+int maxSumIS(int arr[], int n)  
+	{  
+	     int *dp=new int[n]();
+	     int ans=0;
+	     for(int i=0;i<n;i++){
+	         dp[i]=arr[i];
+	         for(int j=0;j<i;j++){
+	             if(arr[i]>arr[j])
+	                dp[i]=max(dp[i],dp[j]+arr[i]);
+	         }
+	         ans=max(ans,dp[i]);
+	   }
+	   return ans;
+	}  
 int main()
 {
     vector<int> ar = {4, 5, 0,2,3};
