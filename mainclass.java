2aeadf260de3ef24cad94f9a3dc59b7b8b42434d
shubham-gupta-17 <39772611+shubham-gupta-17@users.noreply.github.com>
2020-12-16 06:42:12 +0530

    test

---
+++ mainclass.java
@@ -0,0 +1,9 @@
+
+public class mainclass
+{
+public static void main(String [] args)
+{
+int x=10; int y=15; int sum=x+y;
+System.out.println(sum);
+}
+}
---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 15;
+    int sum = x + y;
+    System.out.println(sum);
+  }
+}
